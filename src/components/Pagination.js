ad45120fa20ea88a835a34c6c872c6eeb0984d41
rrih <rsklv@icloud.com>
2020-11-03 06:24:07 +0900

    stylesは使用しない

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,13 +1,13 @@
 import { Link } from "gatsby"
 import React from "react"
-import styles from "./Pagination.module.css"
+// import styles from "./Pagination.module.css"
 
 const Pagination = ({ props }) => {
 	const { pageContext } = props;
 	const { previousPagePath, nextPagePath } = pageContext;
 
 	return (
-		<div className={styles.pagination}>
+		<div>
 			{previousPagePath ? <Link to={previousPagePath}>前のページ</Link> : null }
 			{nextPagePath ? <Link to={nextPagePath}>次のページ</Link> : null }
 		</div>
