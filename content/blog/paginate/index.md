fd45f957a8a8d2dd2cf0d843fca2eb3a606d1f1b
rrih <rsklv@icloud.com>
2020-11-03 05:50:55 +0900

    add article

---
+++ content/blog/paginate/index.md
@@ -0,0 +1,7 @@
+---
+title: paginate機能つけてみる！
+date: "2020-11-03T05:50:00.284Z"
+description: "test"
+---
+
+わからん誰か方法教えて＞＜
--- src/pages/about.js
+++ src/pages/about.js
@@ -17,6 +17,7 @@ const About = () => {
                 </header>
                 <section>
                     ろいです！
+                    うえええええ
                 </section>
             </article>
         </div>
