673ee53aa5d38a3039a114d87c34f7124ded9882
rrih <rsklv@icloud.com>
2020-12-22 02:53:08 +0900

    update

--- src/utils/share-sns.jsx
+++ src/utils/share-sns.jsx
@@ -9,33 +9,59 @@ import {
     HatenaIcon,
 } from "react-share";
 import React from "react";
+import styled from "styled-components";
+
+// あんまりよろしくなさそう
+// 密結合になってる
+const ShareIconStyle = styled.div`
+  button {
+    border: solid 3px #f8f9fa !important;
+    border-radius: 50%;
+    svg {
+      circle {
+        fill: #212529;
+      }
+      path {
+        fill: #f8f9fa;
+      }
+      :hover {
+        circle {
+          fill: #f8f9fa;
+        }
+        path {
+          fill: #212529;
+        }
+      }
+    }
+  }
+`
 
 export const ShareSns = ({ articleUrl, articleTitle }) => {
     return (
         <div className="d-flex justify-content-around py-3">
-            <div>
+            <ShareIconStyle>
                 <FacebookShareButton url={articleUrl}>
                     <FacebookIcon size={60} round />
                 </FacebookShareButton>
-            </div>
+            </ShareIconStyle>
 
-            <div>
+            <ShareIconStyle>
                 <LineShareButton url={articleUrl}>
                     <LineIcon size={60} round />
                 </LineShareButton>
-            </div>
+            </ShareIconStyle>
 
-            <div>
+            <ShareIconStyle>
                 <TwitterShareButton title={articleTitle} via="rrih_dev" url={articleUrl}>
                     <TwitterIcon size={60} round />
                 </TwitterShareButton>
-            </div>
+            </ShareIconStyle>
 
-            <div>
+            <ShareIconStyle>
                 <HatenaShareButton title={articleTitle} url={articleUrl}>
                     <HatenaIcon size={60} round />
                 </HatenaShareButton>
-            </div>
+            </ShareIconStyle>
 
         </div>
     );
