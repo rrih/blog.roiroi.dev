96128fe91b82ea593741d98db651f608aa695049
rrih <rsklv@icloud.com>
2021-03-06 18:34:15 +0900

    highlightjsインストール、とりあえずgithubのコードハイライト適用させてみる

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import "highlight.js/styles/github.css"
 import { useEffect } from 'react'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
