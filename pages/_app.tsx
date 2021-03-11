1ac1321e5cedc4c81b4c63c59f1f9351ddfa4081
rrih <rsklv@icloud.com>
2021-03-11 17:34:13 +0900

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,14 @@ import 'highlight.js/styles/a11y-dark.css';
 import { useEffect } from 'react'
 import { useRouter } from 'next/router';
 import { pageview } from  '../lib/gtag';
+import { GA_TRACKING_ID } from '../lib/gtag';
 
 const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   // Google Analyticsをページ遷移時にも対応させる
   const router = useRouter();
   useEffect(() => {
+    console.log(process.env.NODE_ENV)
+    console.log(`front: ${GA_TRACKING_ID}`)
     const handleRouteChange = (url: string) => {
       pageview(url);
     };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { GA_TRACKING_ID } from '../lib/gtag';
 
 class CustomDocument extends Document {
   render(): JSX.Element {
+    console.log(GA_TRACKING_ID)
     return (
       <Html lang='ja'>
         <Head>
