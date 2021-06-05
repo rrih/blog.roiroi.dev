53a2524f46a10be24a6cfe6366d558830700a4d7
rrih <kawahara.ryohei@lancers.co.jp>
2021-06-05 15:33:43 +0900

    wip

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,14 @@ const Header = () => {
     <section className="test-center mt-16 mb-16 md:mb-12">
       <div className="text-center text-6xl font-bold tracking-tighter leading-tight">
         <Link href="/">
-          ろいろぐ
+          <a
+            style={{
+              textDecoration: 'none'
+              
+            }}
+          >
+            ろいろぐ
+          </a>
         </Link>
       </div>
       <div className="text-center mt-5">
